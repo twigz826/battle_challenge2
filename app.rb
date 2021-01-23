dce0008b4b72905a4353cad935dd78a623d8e85c
twigz826 <thomastwigden@gmail.com>
2021-01-22 22:58:34 -0300

    Sinatra: using the Chrome Devtools

--- app.rb
+++ app.rb
@@ -12,3 +12,18 @@ end
 get '/heya' do
   "HEYA!"
 end
+
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/cat-form' do
+  erb :cat_form
+end
