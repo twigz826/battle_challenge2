9c99c7bbac3adacb784798b5a9df4d6ee54b3369
twigz826 <thomastwigden@gmail.com>
2021-01-26 09:00:52 -0300

    Post-redirect-get path finished

--- battle/app.rb
+++ battle/app.rb
@@ -1,6 +1,9 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/test' do
     'Testing infrastructure working!'
   end
@@ -10,8 +13,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb(:play)
   end
 
