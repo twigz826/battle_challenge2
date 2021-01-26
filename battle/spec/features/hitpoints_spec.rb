940d624231f42d8902c8005979b67549483dfc2a
twigz826 <thomastwigden@gmail.com>
2021-01-26 09:03:59 -0300

    Post-redirect-get path finished

---
+++ battle/spec/features/hitpoints_spec.rb
@@ -0,0 +1,9 @@
+feature 'Hitpoints' do
+  scenario 'See player 2s hitpoints' do
+    visit('/')
+    fill_in :player_1_name, :with => "William Wallace"
+    fill_in :player_2_name, :with => "Sam Pellegrino"
+    click_button "Submit"
+    expect(page).to have_text("60HP")
+  end
+end
